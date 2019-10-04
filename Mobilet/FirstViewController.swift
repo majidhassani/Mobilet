f46ab91977fa06cf5643852d1a2b9a6860985720
majid.hassani73 <majid.hassani73@outlook.com>
2019-10-04 12:43:36 +0330

    Initial Commit

---
+++ Mobilet/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Mobilet
+//
+//  Created by saeed on 10/4/19.
+//  Copyright © 2019 Majid Hassani. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Mobilet/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Mobilet
+//
+//  Created by saeed on 10/4/19.
+//  Copyright © 2019 Majid Hassani. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
